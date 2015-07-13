3e558e3bb5f10e8b23a8f14e25b220e5909f840f
makimoto <tkymakimoto@gmail.com>
2015-07-14 00:28:55 +0900

    Add "scalar.h"

---
+++ include/scalar.h
@@ -0,0 +1,21 @@
+/*
+ * scalar.h
+ *
+ *  Created on: 2015/07/14
+ *      Author: tmakimoto
+ */
+
+#ifndef SCALAR_H_
+#define SCALAR_H_
+
+namespace scl {
+
+template<typename Dimension,typename Type>
+struct scalar{
+};
+
+}  // namespace scl
+
+
+
+#endif /* SCALAR_H_ */
